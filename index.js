b2dfc7bb01ccf9a895f3e2ae96dec8d07e24af10
Arif Çakıroğlu <cakirogluarif@gmail.com>
2015-10-22 10:00:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 module.exports= function(){
   var d = new Date().getTime();
-  var guid = 'xxxxxxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  var guid = 'xxxxxxxxxxxxxxxxxxxx'.replace(/[x]/g, function(c) {
     var r = (d + Math.random()*16)%16 | 0;
     d = Math.floor(d/16);
     return (c=='x' ? r : (r&0x3|0x8)).toString(16);
