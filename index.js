91d67317be7a46dddad7e42d2bbbdbeb596effd1
Salih Sağdilek <salihsagdilek@gmail.com>
2016-11-22 11:27:51 +0300

    a-z support

--- index.js
+++ index.js
@@ -7,11 +7,10 @@
  */
 
 module.exports= function(){
-  var d = new Date().getTime();
   var guid = 'xxxxxxxxxxxxxxxxxxxx'.replace(/[x]/g, function(c) {
-    var r = (d + Math.random()*16)%16 | 0;
-    d = Math.floor(d/16);
-    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    var d = new Date().getTime();
+    var r = (d + Math.random()*36)%36 | 0;
+    return (c=='x' ? r : (r&0x3|0x8)).toString(36);
   });
   //console.log("this is a guid %s",guid);
   return guid
